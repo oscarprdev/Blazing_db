7ee6cd9914764a94c69a000858f79ba74948216a
oscarprdev <vlcoscar8@gmail.com>
2024-08-22 17:23:44 +0200

    feat: include prettierrc

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -1,6 +1,6 @@
 
 import type { Config } from "tailwindcss";
-import sharedConfig from "@repo/tailwind-config";
+import sharedConfig from "../../packages/tailwind-config/tailwind.config";
 
 const config: Pick<Config, "content" | "presets"> = {
   content: ["./src/app/**/*.tsx"],
