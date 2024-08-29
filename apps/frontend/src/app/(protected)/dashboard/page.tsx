3d17ae22834ccc2251cc4f86e1eeb065dca3656b
oscarprdev <vlcoscar8@gmail.com>
2024-08-29 18:56:44 +0200

    feat: test1

--- apps/frontend/src/app/(protected)/dashboard/page.tsx
+++ apps/frontend/src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,11 @@
 'use server';
 
+import { auth } from '@/src/auth';
 import AccordionProjects from '@/src/components/accordion-projects';
 import AccordionQueries from '@/src/components/accordion-queries';
 import Aside from '@/src/components/aside';
 import HeaderDashboard from '@/src/components/header-dashboard';
-import { TablesView, TablesViewFallback } from '@/src/components/tables-view';
-import { Suspense } from 'react';
+import { TablesView } from '@/src/components/tables-view';
 
 async function DashboardPage({
 	searchParams: { projectId },
@@ -14,6 +14,8 @@ async function DashboardPage({
 		projectId: string | undefined;
 	};
 }) {
+	const session = await auth();
+
 	return (
 		<div className="flex flex-col m-0 h-screen p-3">
 			<HeaderDashboard />
@@ -23,9 +25,10 @@ async function DashboardPage({
 					{projectId && <AccordionQueries projectId={projectId} />}
 				</Aside>
 				<section className="relative grid place-items-center max-w-full flex-1 overflow-hidden">
-					<Suspense key={projectId} fallback={<TablesViewFallback />}>
-						{projectId && <TablesView projectId={projectId} />}
-					</Suspense>
+					{/* <Suspense key={projectId} fallback={<TablesViewFallback />}>
+						{projectId && session?.user?.id && <TablesView projectId={projectId} token={session.user.id} />}
+					</Suspense> */}
+					{projectId && session?.user?.id && <TablesView projectId={projectId} token={session.user.id} />}
 				</section>
 			</main>
 		</div>
--- apps/frontend/src/components/tables-view.tsx
+++ apps/frontend/src/components/tables-view.tsx
@@ -1,33 +1,37 @@
-'use server';
+'use client';
 
-import { auth } from '../auth';
 import { describeProject } from '../lib/db/queries';
 import { isError } from '../lib/utils';
 import { FormAi, FormAiSkeleton } from './form-ai';
 import ProjecttTitle from './project-title';
 import TablesViewFlow from './tables-view-flow';
 import { IconLoader2 } from '@tabler/icons-react';
+import { useQuery } from '@tanstack/react-query';
+import { toast } from 'sonner';
 
-async function TablesView({ projectId }: { projectId: string }) {
-	const session = await auth();
-	const describeProjectResponse = await describeProject({ projectId, userToken: session?.user?.id || '' });
+function TablesView({ projectId, token }: { projectId: string; token: string }) {
+	const { data, isLoading } = useQuery({
+		queryKey: ['describeProject', projectId],
+		queryFn: () => describeProject({ projectId, userToken: token }),
+	});
+
+	if (data && isError(data)) {
+		toast.error(data.error);
+	}
 
 	return (
 		<>
-			{!isError(describeProjectResponse) ? (
+			{data && !isError(data) ? (
 				<>
-					<TablesViewFlow
-						tables={describeProjectResponse.success.tables}
-						projectTitle={describeProjectResponse.success.title}>
-						<ProjecttTitle title={describeProjectResponse.success.title} />
+					<TablesViewFlow tables={data.success.tables} projectTitle={data.success.title}>
+						<ProjecttTitle title={data.success.title} />
 					</TablesViewFlow>
-					<FormAi
-						tables={describeProjectResponse.success.tables}
-						type={describeProjectResponse.success.type}
-					/>
+					<FormAi tables={data.success.tables} type={data.success.type} />
 				</>
+			) : data ? (
+				<p className="text-center text-xs text-destructive">{data.error}</p>
 			) : (
-				<p className="text-xs text-destructive">{describeProjectResponse.error}</p>
+				isLoading && <TablesViewFallback />
 			)}
 		</>
 	);
